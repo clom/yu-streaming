7ec77fda4dc3e8b22eb98f4e74e70797affd4617
Kento Nomiyama <nomiyama@mail.miuna.net>
2021-04-08 23:39:11 +0900

    fix view player\

--- resources/views/player_vod.blade.php
+++ resources/views/player_vod.blade.php
@@ -74,7 +74,7 @@
             <a class="nav-link" href="/">Top <span class="sr-only"></span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./vod">Vod <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/vod">Vod <span class="sr-only">(current)</span></a>
           </li>
         </ul>
       </div>
